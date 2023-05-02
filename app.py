0416f9e70217c17e4974c91eb864fb396e531757
Nikogranet <118061679+Nikogranet@users.noreply.github.com>
2023-05-02 23:03:22 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import graphviz
+import networkx as net
+
+with open("list.txt", 'r') as file:
+    graph_data = graphviz.Graph()
+    graph = []
+    const =0
+    while (const == 0):
+        tops = file.readline()
+        top = tops.split()
+        if len(top) == 2:
+            graph_data.edge(top[0],top[1])
+            graph.append(top)
+        elif len(top) == 1:
+            graph_data.edge(top[0])
+            graph.append(top)
+        else:
+            print("You should enter 1 or 2 tops in every line")
+            const = 1
+
+    print(graph)
+graph_data.view()
